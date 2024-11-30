6d41bfdc4b6c3e23da2bf01d0927a0bcf7df2e81
eosgnb <eosuganob@iskolarngbayan.pup.edu.ph>
2024-11-30 22:53:57 +0800

    init

--- resources/frontend/index.js
+++ resources/frontend/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Main from "./pages/Main";
+import Theme from "./index/ThemeProvider";
+import { RouterProvider } from "react-router-dom";
+import router from "./index/router";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Main />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <Theme>
+            <RouterProvider router={router} />
+        </Theme>
+    </React.StrictMode>,
+);
